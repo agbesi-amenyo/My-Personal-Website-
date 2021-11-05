8e25af46e6878c85fa8f04cef5f7b8425493208e
Agbesi Amenyo <56748821+agbesi-amenyo@users.noreply.github.com>
2021-11-05 22:14:52 +0100

    all done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,8 +8,44 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 const Footer = () => {
   return (
     <Section>
+   <FooterWrapper>
+     <LinkList>
+     {/* Phone Number */}
+     <LinkColumn>
+     <LinkTitle>Call</LinkTitle>
+     <LinkItem  href="tel:(+33)758464554" >(+33)758464554</LinkItem>
+     </LinkColumn>
+     {/* Email */}
+     <LinkColumn>
+     <LinkTitle>E-mail</LinkTitle>
+     <LinkItem  href="mailto:agbesiamenyo@gmail.com" >agbesiamenyo@gmail.com</LinkItem>
+     </LinkColumn>
+     </LinkList>
 
-    </Section>
+     <SocialIconsContainer>
+     <CompanyContainer>
+       <Slogan>Improving one project at a time </Slogan>
+     </CompanyContainer>
+     {/* SOCIALS  */}
+     {/* <SocialIconsContainer> */}
+{/* Github */}
+     <SocialIcons  href ="https://github.com/a-amenyo">
+       <AiFillGithub size = "3rem " />
+     </SocialIcons>
+{/* LinkedIn */}
+     <SocialIcons  href ="https://www.linkedin.com/in/agbesi-amenyo/">
+       <AiFillLinkedin size = "3rem " />
+     </SocialIcons>
+{/* Twitter*/}
+     <SocialIcons  href ="https://twitter.com/codewithgrids?s=20">
+       <AiFillTwitterCircle size = "3rem " />
+     </SocialIcons>
+     </SocialIconsContainer>
+
+     {/* </SocialIconsContainer> */}
+    
+   </FooterWrapper>
+   </Section>
   );
 };
 
